fd42efbafe7e38d98b234dac2ab0abee4409ef18
Viraj Mehta <thebigmehtaphor@gmail.com>
2015-08-14 11:22:27 -0700

    marked up code a littlewq

--- hasItDropped.py
+++ hasItDropped.py
@@ -5,9 +5,15 @@ import time
 import re
 import subprocess
 
+'''This module is designed to request all the new content from reddit.com/r/hiphopheads every 10 minutes.
+It then matches every submission's title to a regular expression that would fit any submission of Frank's new album that fits the subreddit's rules.
+If it finds the album, it sends a text with the link to everyone in the phone book in the source.
+It seems to be working, but the only real test is the one that matters.
+I purposely avoided PRAW here beacue I wanted to get a feel for using requests for this type of work, since it's more genrally applicable.
+It was also a challenge to deal with authorizing this program'''
 
 def getToken():
-	'''Gets OAuth token from reddit and saves as the file '.token'. also returns the token.  There is no protection here.'''
+	'''Gets OAuth token from reddit and saves as the file '.token'. also returns the token.  There is no protection here for the file.'''
 	f = open('.token', 'w')
 	client_ID = 'LAFe-EVyQRQwJA'
 	client_secret = 'Ci3byZwr0MXSqWZNrGuCFtbs_4s'
@@ -21,6 +27,9 @@ def getToken():
 	return data[u'access_token']
 
 def trawl(postDict, lastTime, headers):
+	'''When passed a dictionary of the decoded JSON that reddit sends, it looks for Frank's album.  
+	If it doesn't find it, it requests more old posts until the last one it read in the last trawl to make sure it doesn't miss any.  Then it returns false.
+	If it finds the album, it returns the link to it.'''
 	boys_dont_cry = re.compile('\[FRESH\].*(Frank|frank|FRANK).*(OCEAN|Ocean|ocean).*')
 	reTrawl = True
 	while reTrawl:
@@ -38,6 +47,7 @@ def trawl(postDict, lastTime, headers):
 
 
 def request(headers):
+	'''This function is the wrapper for the request and reauthentication for this app'''
 	new_posts = requests.get('https://oauth.reddit.com/r/hiphopheads/new/.json', headers=headers)
 	if new_posts.status_code == 429 or new_posts.status_code == 401:
 		print('429/401: trying again')
@@ -52,7 +62,7 @@ def main():
 	import pdb; pdb.set_trace()
 	dropped = False
 	f = open('.token', 'r')
-	token = f.read().splitlines()
+	token = f.read().splitlines()#checks for old token.  Mostly for testing purposes, since when it runs it will hold the token in memory I think
 	f.close()
 	headers = {'Authorization': 'bearer ' + token[0], 'User-Agent': 'hasItDropped by viraj', 'limit': '1'}
 	trawlNum = 0
@@ -62,19 +72,18 @@ def main():
 	while not dropped:
 		new_posts = request(headers)
 		trawlNum += 1
-
-		print 'Trawl {} complete'.format(trawlNum)
-		
 		data = new_posts.json()
 		dropped = trawl(data, lastTime, headers)
-		lastTime = data['data']['children'][0]['data']['created_utc']
+		print 'Trawl {} complete'.format(trawlNum)
+		
+		lastTime = data['data']['children'][0]['data']['created_utc'] #saves freshest post looked at for future endpoint
 
-		time.sleep(600)
+		time.sleep(600)	#wait 10 minutes
 
 	phoneBook = {'Viraj': '5129631439', 'David': '4256477687'}
 	for name in phoneBook:
 		message = "Hi, {}!  This is Viraj Mehta's hasItDropped app telling you that Frank Ocean has dropped at {}".format(name, dropped)
-		subprocess.call(['osascript', 'textScript.scpt', phoneBook[name], message])
+		subprocess.call(['osascript', 'textScript.scpt', phoneBook[name], message])	#send a text
 
 	return
 
